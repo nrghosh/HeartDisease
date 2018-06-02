30d41d8bea3b79225358b87869520ed6813e87e2
Nikhil Ghosh <nrghosh@users.noreply.github.com>
2018-06-01 23:07:46 -0700

    Add steps to SVM

--- svm.R
+++ svm.R
@@ -1,1 +1,37 @@
+# Part 1: Data Prep
+load("/Users/nikhilghosh/Desktop/DM:ML/asgn5/heart.rdata")
+set.seed(1234)
+index <- sample(nrow(heart), 0.75*nrow(heart))
+train <- heart[index,]
+test <- heart[-index,]
 
+
+# Part 2: Support Vector Machine
+# Train
+library(e1071)
+set.seed(1234)
+fit.svm <- svm(output ~ ., data=train)
+summary(fit.svm)
+prediction <- predict(fit.svm, train)
+table(train$output, prediction)
+
+# Test
+prediction <- predict(fit.svm, test)
+table(test$output, prediction)
+prop.table(table(prediction==test$output))
+
+# Tuning
+Gamma <- 10^(-5:0)
+Cost <- 10^(0:5)
+tune <-  tune.svm(output ~ ., data=train,
+                  gamma=Gamma, cost=Cost)
+summary(tune)
+
+# Tuned Model
+tuned.svm <- svm(output ~., data=train,
+                 gamma=.01, cost=1)
+prediction <- predict(tuned.svm, test)
+
+# Evaluate
+library(caret)
+confusionMatrix(prediction, test$output, positive="Present")
